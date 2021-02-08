89ba3e29811a0bf199f02b67572820a3a7bab1ab
Tuomas Kareinen <tkareine@gmail.com>
2021-02-08 21:01:37 +0200

    Increase test coverage slightly

--- src/__tests__/slug.test.mjs
+++ src/__tests__/slug.test.mjs
@@ -3,5 +3,5 @@ import { slug } from "../slug.mjs"
 
 test("slug", (t) => {
   t.is(slug("Hacker's Tiny Slide Deck"), "hacker-s-tiny-slide-deck")
-  t.is(slug(" \t a →\nöä\tBc D\n"), "a------bc-d")
+  t.is(slug(" \t a1 →\nöä\tBc 9D\n"), "a1------bc-9d")
 })
