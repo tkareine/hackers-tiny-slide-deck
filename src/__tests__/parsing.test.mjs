e394a6cd2ac49eb26833a7105fe1d88b0e0ee0bb
Tuomas Kareinen <tkareine@gmail.com>
2022-01-16 10:18:27 +0200

    Use Ava macro to run test cases separately

--- src/__tests__/parsing.test.mjs
+++ src/__tests__/parsing.test.mjs
@@ -1,25 +1,34 @@
 import test from "ava"
 import { parseIntInRange } from "../parsing.mjs"
 
-test("parseIntInRange", (t) => {
-  const f = parseIntInRange.bind(undefined, 2, 6)
+const testParseIntInRange = (() => {
+  const defaultMin = 2
+  const defaultMax = 6
 
-  t.is(f("0"), 2)
-  t.is(f("1"), 2)
-  t.is(f("2"), 2)
-  t.is(f("3"), 3)
-  t.is(f("5"), 5)
-  t.is(f("6"), 6)
-  t.is(f("7"), 6)
-  t.is(f("-1"), 2)
-  t.is(f("-2"), 2)
-  t.is(f("2.1"), 2)
-  t.is(f("2.9"), 2)
-  t.is(f(""), 2)
-  t.is(f("  \t \n "), 2)
-  t.is(f(null), 2)
-  t.is(f(undefined), 2)
-  t.is(f("asdf"), 2)
-  t.is(f("asdf3"), 2)
-  t.is(f("3asdf"), 3)
-})
+  return test.macro({
+    exec: (t, inputStr, expected, min = defaultMin, max = defaultMax) => {
+      t.is(parseIntInRange(min, max, inputStr), expected)
+    },
+    title: (providedTitle = "", inputStr, expected, min = defaultMin, max = defaultMax) =>
+      `${providedTitle} parseIntIntInRange("${inputStr}", ${min}, ${max}) = ${expected}`.trim(),
+  })
+})()
+
+test(testParseIntInRange, "0", 2)
+test(testParseIntInRange, "1", 2)
+test(testParseIntInRange, "2", 2)
+test(testParseIntInRange, "3", 3)
+test(testParseIntInRange, "5", 5)
+test(testParseIntInRange, "6", 6)
+test(testParseIntInRange, "7", 6)
+test(testParseIntInRange, "-1", 2)
+test(testParseIntInRange, "-2", 2)
+test(testParseIntInRange, "2.1", 2)
+test(testParseIntInRange, "2.9", 2)
+test(testParseIntInRange, "", 2)
+test(testParseIntInRange, "  \t \n ", 2)
+test(testParseIntInRange, null, 2)
+test(testParseIntInRange, undefined, 2)
+test(testParseIntInRange, "asdf", 2)
+test(testParseIntInRange, "asdf3", 2)
+test(testParseIntInRange, "3asdf", 3)
--- src/__tests__/slug.test.mjs
+++ src/__tests__/slug.test.mjs
@@ -1,7 +1,12 @@
 import test from "ava"
 import { slug } from "../slug.mjs"
 
-test("slug", (t) => {
-  t.is(slug("Hacker's Tiny Slide Deck"), "hacker-s-tiny-slide-deck")
-  t.is(slug(" \t a1 →\nöä\tBc 9D\n"), "a1------bc-9d")
+const testSlug = test.macro({
+  exec: (t, inputStr, expected) => {
+    t.is(slug(inputStr), expected)
+  },
+  title: (providedTitle = "", inputStr, expected) => `${providedTitle} slug(${inputStr}) = ${expected}`.trim(),
 })
+
+test(testSlug, "Hacker's Tiny Slide Deck", "hacker-s-tiny-slide-deck")
+test(testSlug, " \t a1 →\nöä\tBc 9D\n", "a1------bc-9d")
